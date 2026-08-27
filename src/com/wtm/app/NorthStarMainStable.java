2b92b4ca2360a318d9cacfcc28afd5854de7f37e
Jonathan Stotts <111158578+Jonbstotts@users.noreply.github.com>
2026-08-27 14:05:57 -0500

    Enable music workspace polish guard

--- src/com/wtm/app/NorthStarMainStable.java
+++ src/com/wtm/app/NorthStarMainStable.java
@@ -20,6 +20,7 @@ public final class NorthStarMainStable {
         invoke("com.wtm.modular.ui.TrustedEmailUiGuard","install");
         invoke("com.wtm.modular.ui.DashboardGlyphArtGuard","install");
         invoke("com.wtm.modular.ui.MusicModuleGuard","install");
+        invoke("com.wtm.modular.ui.MusicWorkspacePolishGuard","install");
         invoke("com.wtm.modular.core.ModularBootstrap","start");
         invoke("com.wtm.firstparty.NetworkGeocodingService","start");
         Main.main(args);
